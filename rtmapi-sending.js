ec0dbf1ba5b89876f35f1c465256b25426793766
Monica Danesa <monicadanesa@gmail.com>
2018-02-14 23:48:38 +0700

    no message

--- rtmapi-sending.js
+++ rtmapi-sending.js
@@ -2,7 +2,7 @@ const RtmClient  = require('@slack/client').RtmClient;
 const WebClient  = require('@slack/client').WebClient;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-const bot_token = 'xoxp-313800941633-314350545762-314378833392-94259b6001554e74306ea34c09661256';
+const bot_token = '';
 const rtm       = new RtmClient(bot_token);
 const web       = new WebClient(bot_token);
 
--- sample_test.js
+++ sample_test.js
@@ -1,7 +1,7 @@
 const { WebClient } = require('@slack/client');
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
-const token ='xoxb-314978718356-U40qcGse3XEDRonEdu2GevyL';
+const token ='';
 
 const web = new WebClient(token);
 
